ad1d97a2cba52e756585d48169a5376253f23937
Matthew Rampen <rampen.matthew@gmail.com>
2023-01-17 11:28:48 -0500

    Code Copied transition/popup is complete, not entirely sure if I am set on the stylings of them.

--- README.md
+++ README.md
@@ -13,9 +13,10 @@
     1. Sort out which smart contract interactions to tap into.
 5. COMPONENTS
 
-    1. Finish up CodeOverlayProvider. Code content is properly contained now, and gets applied styles through a script provided by google https://github.com/googlearchive/code-prettify. Need to now do the same and setup how to choose which code is shown depending which instance of code button is clicked. Figure out how to make copy text function work.
+    1. CodeOverlayProvider I believe is finished, for now at least until I think some stylings are ugly.
     2. Do I want different colors for the code.svg when toggling light/dark theme? Might be able to achieve this relatively painlessly if use ThemeContext inside the ShowCode.tsx component.
     3. For Header.tsx consider how tailwindcss has their header setup. transparent with a blur, and it takes the background of the underlying. Gets a deeper shade as soon as a scroll occurs.?
+    4. Create new generic CCButton that holds the main styling. The same styling is repeated any time a button is made except for ShowCodeButton, but this can also utilize the CCButton generic component when created.
 
 6. SCRIPTS
-    1. Make CreateCodeSnippets.ts more dynamic so that it makes the constants name unqiue depending which .md is being processed. Likely need to make a key/value mapping then just for each through it. Make the constant names the Key, and build out one giant string to write to the file in the loop and do one write after the loop is finished.
+    1. CreateCodeSnippets is good to go I think.
--- components/context/CodeOverlayProvider.tsx
+++ components/context/CodeOverlayProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useRef, useState } from "react";
+import { createContext, useEffect, useMemo, useRef, useState } from "react";
 import Image from "next/image";
 import { HeaderCode, IndexCode } from "../../constants/CodeSnippets";
 
@@ -22,44 +22,39 @@ export default function CodeOverlayProvider({
     children,
 }: ICodeOverlayProviderProps) {
     const [codeOverlay, setCodeOverlay] = useState<CodeOverlay>(null);
-    const [showOverlay, setShowOverlay] = useState(false);
+    const [codeText, setCodeText] = useState<string>("");
+    const [copied, setCopied] = useState(false);
+    const overlayDiv = useRef<HTMLDivElement>(null);
+    const codeDiv = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (showOverlay) {
+    useMemo(() => {
+        //setCodeText accordingly
+        switch (codeOverlay) {
+            case "header": {
+                setCodeText(HeaderCode);
+                break;
+            }
+            case "index": {
+                setCodeText(IndexCode);
+                break;
+            }
+        }
+        if (typeof document !== "undefined") {
             const script = document.createElement("script");
+
             script.src =
                 "https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js";
             document.body.append(script);
         }
-    }, [showOverlay]);
+    }, [codeOverlay]);
 
     function setOverlay(codeOverlay: CodeOverlay) {
         setCodeOverlay(codeOverlay);
     }
 
     function toggleOverlay() {
-        setShowOverlay(!showOverlay);
-    }
-
-    function copyCode() {
-        //not sure how to do this just yet
-    }
-
-    function determineCode() {
-        let codeText: string = "";
-
-        switch (codeOverlay) {
-            case "header": {
-                codeText = HeaderCode;
-                break;
-            }
-            case "index": {
-                codeText = IndexCode;
-                break;
-            }
-        }
-
-        return <div dangerouslySetInnerHTML={{ __html: codeText }} />;
+        overlayDiv.current!.classList.toggle("hidden");
+        setCopied(false);
     }
 
     return (
@@ -67,41 +62,61 @@ export default function CodeOverlayProvider({
             value={{ codeOverlay, toggleOverlay, setOverlay }}
         >
             <div className="relative">
-                {showOverlay ? (
-                    <div className="absolute z-20 h-screen w-screen items-center justify-center backdrop-blur-md">
-                        <div className="flex flex-col h-screen min-w-[380px] p-3 sm:py-24 sm:px-28 xl:px-52 2xl:py-24 2xl:px-80">
-                            <div className="flex space-x-3 justify-end">
-                                <button
-                                    className="rounded-md shadow-lg p-3 hover:bg-opacity-25 hover:dark:bg-opacity-25 hover:bg-lbtn-hov hover:dark:bg-dbtn-hov bg-ls-back dark:bg-dt-back"
-                                    onClick={copyCode}
-                                >
-                                    <Image
-                                        src="/copy.svg"
-                                        width={20}
-                                        height={20}
-                                        alt="Copy svg"
-                                    />
-                                </button>
-                                <button
-                                    className="rounded-md shadow-lg p-3 hover:bg-opacity-25 hover:dark:bg-opacity-25 hover:bg-lbtn-hov hover:dark:bg-dbtn-hov bg-ls-back dark:bg-dt-back"
-                                    onClick={toggleOverlay}
-                                >
-                                    <Image
-                                        src="/x.svg"
-                                        width={20}
-                                        height={20}
-                                        alt="X(Close) svg"
-                                    />
-                                </button>
+                <div
+                    ref={overlayDiv}
+                    className="absolute z-20 h-screen w-screen items-center justify-center backdrop-blur-md hidden"
+                >
+                    <div className="flex flex-col h-screen min-w-[380px] p-3 sm:py-24 sm:px-28 xl:px-52 2xl:py-24 2xl:px-80">
+                        <div className="flex space-x-3 justify-end">
+                            <div
+                                className={`rounded-md shadow-lg p-3 bg-opacity-90 dark:bg-opacity-90 bg-ls-back dark:bg-dt-back transition-all duration-[1500ms] ease-out	 ${
+                                    copied ? "opacity-100" : "opacity-0"
+                                }`}
+                                onTransitionEnd={() => setCopied(false)}
+                            >
+                                <span className="font-bold text-ls-fore  dark:text-dp-fore">
+                                    Code Copied!
+                                </span>
                             </div>
-                            <div className="mt-3 overflow-scroll p-4 rounded-md shadow-2xl bg-dp-back dark:bg-dt-back">
-                                {determineCode()}
-                            </div>
+                            <button
+                                className="rounded-md shadow-lg p-3 hover:bg-opacity-25 hover:dark:bg-opacity-25 hover:bg-lbtn-hov hover:dark:bg-dbtn-hov bg-ls-back dark:bg-dt-back"
+                                onClick={() => {
+                                    navigator.clipboard.writeText(
+                                        codeDiv.current!.textContent!
+                                    );
+                                    setCopied(true);
+                                }}
+                            >
+                                <Image
+                                    src="/copy.svg"
+                                    width={20}
+                                    height={20}
+                                    alt="Copy svg"
+                                />
+                            </button>
+                            <button
+                                className="rounded-md shadow-lg p-3 hover:bg-opacity-25 hover:dark:bg-opacity-25 hover:bg-lbtn-hov hover:dark:bg-dbtn-hov bg-ls-back dark:bg-dt-back"
+                                onClick={toggleOverlay}
+                            >
+                                <Image
+                                    src="/x.svg"
+                                    width={20}
+                                    height={20}
+                                    alt="X(Close) svg"
+                                />
+                            </button>
+                        </div>
+                        <div className="mt-3 overflow-scroll p-4 rounded-md shadow-2xl bg-dp-back dark:bg-dt-back">
+                            <div
+                                ref={codeDiv}
+                                dangerouslySetInnerHTML={{
+                                    __html: codeText,
+                                }}
+                            />
                         </div>
                     </div>
-                ) : (
-                    <></>
-                )}
+                </div>
+
                 {children}
             </div>
         </CodeOverlayContext.Provider>
