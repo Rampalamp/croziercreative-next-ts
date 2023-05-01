d197789439f15e97eaadc7364c1b4b7df8a150e9
Matthew Rampen <rampen.matthew@gmail.com>
2023-05-01 17:41:33 -0400

    createDataPayload is in a bit of a mess. I think Ihave the start of a payload building properly, need to take a look at some of the data its creating thus far. Its being done in two steps, piece together an income payloadMap, that may or may not need offsets changed, and then the refDataMap, which will need to be used to update and append itself to payloadDataMap.

--- classes/EVMHelper.ts
+++ classes/EVMHelper.ts
@@ -1,3 +1,4 @@
+import { create } from "domain";
 import keccak256 from "keccak256";
 /**
  *
@@ -8,8 +9,8 @@ export function createDataPayload(params: {}): string {
     //assume params coming in the following order - Function name and params claimRank(uint256 term)
     const paramEntries = Object.entries(params);
     let sigParamDataMap: Map<number, string[]>;
-    let signatureParams: string = "";
-    let referenceParams: string = "";
+    let payloadMap: Map<number, string> = new Map<number, string>();
+    let refDataMap: Map<number, string[]> = new Map<number, string[]>();
     let payload: string = "";
     let param: any;
     let paramKey: string;
@@ -39,27 +40,95 @@ export function createDataPayload(params: {}): string {
             }
         } else {
             //parameters of the function signature
-            //we have the sigParamDataMap that we need to fill with proper 32 byte hex data
-            //depending on whether or not the parameter is a value or reference type.
+            //mappedParam[0] will be either static or dynamic
+            //mappedParam[1] will be the actual solidity data type (bool, address, uint etc.)
             const mappedParam = sigParamDataMap!.get(i);
 
-            //determine if param is reference type or value type
+            //determine if param is dynamic type or static type
             //This part can get tricky, and likley more work then I have time to do
             //Going to keep it simple and only handle basic arrays, not multi-dimensional.
             //things to note, the initial 4 selector bytes are not considered when calculating offset bytes
-            //the offset byte for the first reference type in sig params can be easily calculated
+            //the offset byte for the first dynamic type in sig params can be easily calculated
             //params.length x 32
-            //offset for a secondary reference typeneed to be calculated based off the first reference type.
-            //if the first reference type is an array of numbers, each number will push the
-            //offset of the second reference type another 32 bytes down.
-            if (mappedParam![1] === "value") {
-            } else {
-                //reference type
-                //
-            }
-            let param: string = hexifyParameter(paramValue);
 
-            payload = payload + param;
+            //offset for a secondary dynamic type need to be calculated based off the first dynamic type.
+            //if the first dynamic type is an array of numbers, each number will push the
+            //offset of the second dynamic type another 32 bytes down.
+            if (mappedParam![0] === "static") {
+                //for static should be able to just straight add it to the payloadMap object.
+                payloadMap.set(
+                    payloadMap.size,
+                    create32ByteHexString(paramValue)
+                );
+            } else {
+                //dynamic type
+                //add placeholder for offset to payloadMap
+                payloadMap.set(payloadMap.size, "offset" + i.toString());
+                //for now insert a blank hex string
+                //determine complexity/# of dimensions for an array parameter
+                let offsetArray: string[] = [];
+                let dataArray: string[] = [];
+                const paramDataType: string = mappedParam![1];
+                const paramComplexity: number = (
+                    paramDataType.match(/\[\]/g) || []
+                ).length;
+
+                if (paramComplexity === 0) {
+                    //this should be bytes/string if its a dynamic parameter but not explicitly an array in the naming convention [].
+
+                    dataArray.push(create32ByteHexString(paramValue.length));
+                    dataArray.push(create32ByteHexString(paramValue));
+                } else if (paramComplexity === 1) {
+                    //one exception would be if its a string[], which is really a complexity of 2.
+                    if (paramDataType.includes("string")) {
+                        //first encode the actual data
+                        //each entry of array gets 64 bytes
+                        //first 32 bytes are for length of string
+                        //second 32 bytes is the actual character ASCII hex value.
+                        let offsetByteMultiplier: number = paramValue.length;
+                        //set count line in offset array, which at this point should just be the offsetByteMultiplier.
+                        offsetArray.push(
+                            create32ByteHexString(offsetByteMultiplier)
+                        );
+                        for (let k = 0; k < paramValue.length; k++) {
+                            const item = paramValue[k];
+
+                            offsetArray.push(
+                                create32ByteHexString(offsetByteMultiplier * 32)
+                            );
+
+                            offsetByteMultiplier += 2;
+
+                            dataArray.push(create32ByteHexString(item.length));
+                            dataArray.push(create32ByteHexString(item));
+                        }
+                    } else {
+                        //if not a string, no extra offsets needed in this complexity.
+                        //encode the count/number of items in array, then the actual data point.
+                        dataArray.push(
+                            create32ByteHexString(paramValue.length)
+                        );
+                        for (let k = 0; k < paramValue.length.length; k++) {
+                            dataArray.push(
+                                create32ByteHexString(paramValue.length[k])
+                            );
+                        }
+                    }
+                    refDataMap.set(
+                        refDataMap.size,
+                        offsetArray.concat(dataArray)
+                    );
+                } else if (paramComplexity === 2) {
+                } else {
+                    //more then 3 deep, not going to bother for now.
+                }
+
+                //check mappedParam[1] to determine if you are making more offsets (array of arrays, or an array of strings)
+                //If its bytes/strings, get the length
+                //if array of uints/addresses?/ints/bools w/e
+            }
+            console.log(payloadMap);
+            console.log(refDataMap);
         }
     }
     return payload;
@@ -68,7 +137,7 @@ export function createDataPayload(params: {}): string {
 /**
  *
  * @param params String array of the parameters in the function signature
- * @returns Mapping<number,string[]> : key of map is index order of the string[] params passed in. The value string[] will have two strings, first string will always be a padded 32 Hex bytes of 0s, second string will be the param type (value or reference)
+ * @returns Mapping<number,string[]> : key of map is index order of the string[] params passed in. The value string[] will have two strings, first stringwill be the param type (static or dynamic), the second string will be the actual param/data solidity type
  */
 export function createParamDataMapping(
     params: string[]
@@ -80,7 +149,7 @@ export function createParamDataMapping(
 
         const paramType = getSolidityParamType(param);
         //set the key/index to be i+1, this makes it easier inside the createDataPayload function
-        mapping.set(i + 1, [create32ByteHexString("", false), paramType]);
+        mapping.set(i + 1, [paramType, param]);
     }
 
     return mapping;
@@ -88,10 +157,10 @@ export function createParamDataMapping(
 
 /**
  *
- * @param value any type, this value will enter a switch statement which will attempt to determine how it needs to turn the value into hex data with a 32 byte buffer of 0's at the beginning or the end.
+ * @param value any type, this value will enter a switch statement which will attempt to determine how it needs to turn the value into hex data with a 32 byte buffer of 0's at the beginning or the end depending data type.
  * @returns parameter data in hex form with 32 byte hex 0's padding beginning or end.
  */
-export function hexifyParameter(value: any): string {
+export function create32ByteHexString(value: any): string {
     let param: string = "";
     switch (typeof value) {
         case "number":
@@ -101,7 +170,7 @@ export function hexifyParameter(value: any): string {
             break;
         case "string":
             const valString: string = value as string;
-            param = create32ByteHexString(valString, true);
+            param = Buffer.from(valString).toString("hex").padEnd(64, "0");
             break;
         case "boolean":
             const valBool: boolean = value as boolean;
@@ -113,9 +182,7 @@ export function hexifyParameter(value: any): string {
             break;
 
         default:
-            //only going to add a case for arrays of strings or number
-            //could continue to add more exceptions for other possibly data types
-            //too many considerations for all possible parameters smart contracts can take...
+            //I dont think any of this array logic will stay in here.
             if (Array.isArray(value)) {
                 const isStringArray =
                     value.length > 0 &&
@@ -125,7 +192,9 @@ export function hexifyParameter(value: any): string {
 
                 if (isStringArray) {
                     value.forEach((val) => {
-                        param = create32ByteHexString(val, true);
+                        param =
+                            param +
+                            Buffer.from(val).toString("hex").padEnd(64, "0");
                     });
                     break;
                 }
@@ -138,9 +207,18 @@ export function hexifyParameter(value: any): string {
 
                 if (isNumberArray) {
                     value.forEach((val) => {
-                        param = val.toString(16).padStart(64, "0");
+                        param = param + val.toString(16).padStart(64, "0");
                     });
                     break;
+                }
+                //for each new depth of arrays, it will result in new offsets being needed.
+                const isArrayOfArray =
+                    value.length > 0 &&
+                    value.every((val) => {
+                        return Array.isArray(val);
+                    });
+                if (isArrayOfArray) {
+                    //Not sure if I want to bother with this currently.
                 }
             }
             break;
@@ -158,21 +236,5 @@ export function getSolidityParamType(type: string): string {
     //should cover int/uint/bytes+any number (bytes1,bytes2) - indicating not dynamic/reference (for strings), address and bool
     const isValueRegex: RegExp = /(int(?!.*\[\]$))|(bytes\d+)|address|bool/;
     //if isValueRegex evaluates to false, just assume its a reference type
-    return isValueRegex.test(type) ? "value" : "reference";
-}
-
-/**
- *
- * @param data string data that will be turned into hex
- * @param padEnd specifies whether or not to pad the front of end with 64 0s to create a 32 BYTE hex string
- * @returns 32 byte hex string from string parameter, pads end or beginning.
- */
-export function create32ByteHexString(data: string, padEnd: boolean): string {
-    const hexString = Buffer.from(data).toString("hex");
-
-    if (padEnd) {
-        return hexString.padEnd(64, "0");
-    }
-
-    return hexString.padStart(64, "0");
+    return isValueRegex.test(type) ? "static" : "dynamic";
 }
